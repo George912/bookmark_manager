0ebbb4e30715b1ab366bf775722ef1decf35bb38
Нестеров Юрий Алексеевич <YANesterov@bellintegrator.com>
2018-05-24 17:52:31 +0400

    impl icon url parser
    - implement url parse logic

--- core/src/main/java/ru/bellintegrator/content/html/parser/IconUrlParser.java
+++ core/src/main/java/ru/bellintegrator/content/html/parser/IconUrlParser.java
@@ -17,10 +17,15 @@ import java.util.regex.Pattern;
  * <p>
  * Created by YANesterov on 23.05.2018.
  */
+//todo: refactoring, internationalization
 @Service("iconUrlParser")
 public class IconUrlParser {
     private static final Logger LOGGER = Logger.getLogger(IconUrlParser.class);
     private static final String ICON_PATTERN = "(\\.net|\\.png|\\.gif|\\.ico)";
+    private static final String FULL_URL_PATTERN = "\"^((https?:\\\\/\\\\/){1}[a-z0-9._-]*)\"";
+    private static final String INVALID_HREF_EXCEPTION_MESSAGE = "Method arg \"href\" must not be null";
+    private static final String INVALID_URL_EXCEPTION_MESSAGE = "Method arg \"url\" must not be null";
+    private static final String LINK_HREF_SELECTOR = "link[href]";
 
     public IconUrlParser() {
         LOGGER.info("IconUrlParser instance created");
@@ -39,29 +44,51 @@ public class IconUrlParser {
     }
 
     //todo:docs, logger
-    public Elements retrieveLinkElements(String url) throws IllegalArgumentException, IOException {
-        if (url == null)
-            throw new IllegalArgumentException("Method arg \"url\" must not be null");
-        else
-            return retrieveDocument(url).select("link[href]");
+    private Elements retrieveLinkElements(String url) throws IOException {
+        return retrieveDocument(url).select(LINK_HREF_SELECTOR);
     }
 
     //todo:docs, logger
-    public URL rettrieveIconUrl(Elements linkElements) {
-        for (Element src : linkElements) {
-            if (isIconLink(src.text())) {
+    public URL rettrieveIconUrl(String url) throws IllegalArgumentException {
+        if (url == null)
+            throw new IllegalArgumentException(INVALID_URL_EXCEPTION_MESSAGE);
+
+        try {
+            Elements linkElements = retrieveLinkElements(url);
+
+            if (linkElements != null) {
+                for (Element link : linkElements) {
+                    if (isUrlMatchPattern(link.text(), ICON_PATTERN, INVALID_HREF_EXCEPTION_MESSAGE)) {
+                        if (isUrlMatchPattern(link.text(), FULL_URL_PATTERN, INVALID_HREF_EXCEPTION_MESSAGE)) {
+                            return new URL(url);
+                        } else {
+                            String site = retrieveSiteUrl(url, FULL_URL_PATTERN);
+                            return new URL(site + link.text());
+                        }
+                    }
+                }
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         return null;
     }
 
     //todo:docs, logger
-    private boolean isIconLink(String href) {
-        Pattern pattern = Pattern.compile(ICON_PATTERN);
-        Matcher matcher = pattern.matcher(href);
+    private String retrieveSiteUrl(String url, String pattern) {
+        Pattern pat = Pattern.compile(pattern);
+        Matcher matcher = pat.matcher(url);
+        return matcher.group(0);
+    }
+
+    //todo:docs, logger
+    private boolean isUrlMatchPattern(String href, String pattern, String exceptionMsg) {
+        Pattern pat = Pattern.compile(pattern);
+        Matcher matcher = pat.matcher(href);
+
         if (href == null)
-            throw new IllegalArgumentException("Method arg \"href\" must not be null");
+            throw new IllegalArgumentException(exceptionMsg);
         else
-            return matcher.matches();
+            return matcher.find();
     }
 }
